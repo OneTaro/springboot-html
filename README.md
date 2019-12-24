3487c9d622080dc58967c74750a68fefa829116b
wangss <1063889643@qq.com>
2019-12-24 09:24:13 +0800

    create springboot demo

---
+++ README.md
@@ -0,0 +1,17 @@
+### 启动项目
+运行 Application 文件
+
+### 添加其他html文件
+在resources/static 文件夹下新增一个文件夹，将文件放入进去即可
+
+### 端口配置
+端口配置在 application.properties 文件中
+
+### 本机访问
+http://localhost:8080/
+
+### 同事访问（局域网）
+http://你的实际ip:8080/
+
+### 项目使用
+需要安装 idea 和 maven，还有jdk
---
+++ src/main/java/com/ld/html/Application.java
@@ -0,0 +1,18 @@
+package com.ld.html;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName Application
+ * @Description
+ * @Author wangss
+ * @date 2019.12.19 15:53
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
